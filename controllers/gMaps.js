d1e807b5fdcefbc903cc4c7f799070b6bb4c6a87
NrupM <maryenorthrup@gmail.com>
2017-09-18 13:16:35 -0700

    eliminate unnecessary console.logs

--- controllers/gMaps.js
+++ controllers/gMaps.js
@@ -1,6 +1,5 @@
 const request = require('request');
 
-
 function getTransitDirections(req, res) {
     console.log('getTransitDirections via maps controller', req.body);
     const originLat = req.query.originLat;
@@ -8,14 +7,9 @@ function getTransitDirections(req, res) {
     const destination = req.query.destination;
     const mode = req.query.mode;
     const newUrl = `https://maps.googleapis.com/maps/api/directions/json?origin=${originLat},${originLng}&destination=${destination}&key=AIzaSyCnyu2FJac70-X0EXKaoIxVw5RB4luN0uk&mode=${mode}`;
-
-    console.log(newUrl);
     res.header('Access-Control-Allow-Origin', '*');
     request(newUrl, function (error, response, body) {
         const transitRoutes = JSON.parse(body).routes[0];
-        console.log('*!!!!!!!', transitRoutes);
-
-        // ^ res is not defined within this scope
     }).pipe(res);
 }
 
