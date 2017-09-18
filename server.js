a4b97d45a716ae0888288dc14b4dfc47b0277c32
NrupM <maryenorthrup@gmail.com>
2017-09-18 13:16:19 -0700

    eliminate console.logs

--- server.js
+++ server.js
@@ -4,20 +4,11 @@ const cors = require('cors');
 
 const muniController = require('./controllers/muni');
 const gMapsController = require('./controllers/gMaps');
-// import * as uberContoller from './controllers/uber';
-// import * as gMapsController from './controllers/gMaps';
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors({ credentials: true, origin: true }));
-// app.all('/', function (req, res, next) {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header('Access-Control-Allow-Credentials', true);
-//     res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
-//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//     next();
-// });
 
 /** ********
  * ROUTES *
